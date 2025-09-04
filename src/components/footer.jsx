24501cac242e10bffd5a59e1c1033665657c364a
giovanni <giovannitarzonipiccin@gmail.com>
2025-09-04 19:34:31 -0300

    criando footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <footer style={{ textAlign: 'center', padding: '1rem', background: '#f0f0f0' }}>
+      <p>© 2025 - calculadora DevPrime em reactjs e tailwind</p>
+    </footer>
+  );
+};
+
+export default Footer;
